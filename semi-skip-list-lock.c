9e5467fc6dc61758fe0e2cc8594b19cbde3a5adf
DylanSunLei <DylanSunLei@users.noreply.github.com>
2016-04-24 16:16:38 -0400

    Create semi-skip-list-lock.c

---
+++ semi-skip-list-lock.c
@@ -0,0 +1,12 @@
+struct Node {
+int value;
+Node* next;
+Node* skip; // note that skip == next->next
+};
+
+// Please implement a thread-safe implementation of node deletion.
+
+// delete node containing value
+void delete_node(Node* head, int value){
+
+}
