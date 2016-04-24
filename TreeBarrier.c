292ee797d4daf75dbc2285e08553cf6e8fb9454f
DylanSunLei <DylanSunLei@users.noreply.github.com>
2016-04-24 14:43:39 -0400

    {

--- TreeBarrier.c
+++ TreeBarrier.c
@@ -1,1 +1,16 @@
+struct Barrier_t {
+  LOCK lock;
+  int entering_counter;
+  int leaving_counter;
+  int flag;
+  
+  Barrier_t *left;
+  Barrier_t *right;
+}
+
+
+struct Barrier_Tree
+
+void Barrier(Barrier_t* b, int p)
+
 
