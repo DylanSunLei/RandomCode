070b55fad62d634e94d484a8f3a0bf12a6216db3
DylanSunLei <DylanSunLei@users.noreply.github.com>
2016-04-24 15:00:16 -0400

    Update TreeBarrier.c

--- TreeBarrier.c
+++ TreeBarrier.c
@@ -2,7 +2,7 @@ struct Barrier_t {
   LOCK lock;
   int entering_counter;  //initialize to 0
   int leaving_counter;
-  int flag; //padded to sti on it's own cache line
+  int flag; //padded to sit on it's own cache line
   
   Barrier_t *left;
   Barrier_t *right;
@@ -15,7 +15,7 @@ struct Barrier_Tree{
 
 //assume P is 2^k
 void Barrier(Barrier_Tree* root){
-  
+  //DFS lookup to a leaf barrier which enter_count < 2;
 }
 
 
